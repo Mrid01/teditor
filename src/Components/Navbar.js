14be3a32d50c340a812c17c57a3a926a60b5d05c
Mrid01 <59385288+Mrid01@users.noreply.github.com>
2022-05-19 20:25:44 +0530

    Update Navbar.js
    added the mobile view button for the navbar contents.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 export default function Navbar(props) {
   return (
     <>
@@ -11,17 +10,30 @@ export default function Navbar(props) {
           <a className="navbar-brand" href="/">
             {props.title}
           </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            
-            <div className={`form-check form-switch ms-auto mb-1 mb-lg-1 mx-4 text-${props.mode==='light'?'dark':'light'} `}>
+            <div
+              className={`form-check form-switch ms-auto mb-1 mb-lg-1 mx-4 text-${
+                props.mode === "light" ? "dark" : "light"
+              } `}
+            >
               <input
                 className="form-check-input "
                 type="checkbox"
                 onClick={props.toggleMode}
                 role="switch"
                 id="flexSwitchCheckDefault"
-                
               />
               <label
                 className="form-check-label"
